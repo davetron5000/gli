d33d493307d2c62fa4c67363d7881e9ad56c47f9
David Copeland <davetron5000@gmail.com>
2009-01-25 18:11:13 -0500

    fleshing out command more.
    I'm not sure the easiest way to share code between the module and
    the command, nor if this may be an indication that my design is bad, but
    it seems to make sense, since the whole idea is that a command takes
    switch and flags, but our "global command" takes also commands

--- lib/gli.rb
+++ lib/gli.rb
@@ -2,19 +2,11 @@ module GLI
   extend self
 
   # describe the next switch, flag, or command
-  def desc(description)
-    @@next_desc = description
-  end
-
+  def desc(description); @@next_desc = description; end
   # describe the argument name of the next flag
-  def arg_name(name)
-    @@next_arg_name = name
-  end
-
+  def arg_name(name); @@next_arg_name = name; end
   # set the default value of the next flag
-  def default_value(val)
-    @@next_default_value = val
-  end
+  def default_value(val); @@next_default_value = val; end
 
   # Create a flag, which is a switch that takes an argument
   def flag(names)
@@ -81,10 +73,43 @@ module GLI
   class Command < CommandLineToken
     def initialize(names,description)
       super(names,description)
+      clear_nexts
+    end
+
+    def flags; @flags ||= {}; end
+    def switches; @switches ||= {}; end
+
+    # describe the next switch or flag
+    def desc(description); @next_desc = description; end
+    # describe the argument name of the next flag
+    def arg_name(name); @next_arg_name = name; end
+    # set the default value of the next flag
+    def default_value(val); @next_default_value = val; end
+
+    def flag(names)
+      flag = Flag.new(names,@next_desc,@next_arg_name,@next_default_value)
+      flags[flag.name] = flag
+      clear_nexts
+    end
+
+    # Create a switch
+    def switch(names)
+      switch = Switch.new(names,@next_desc)
+      switches[switch.name] = switch
+      clear_nexts
+    end
+
+    def action
     end
 
     def self.name_as_string(name)
       name.to_s
+    end
+
+    def clear_nexts
+      @next_desc = nil
+      @next_arg_name = nil
+      @next_default_value = nil
     end
   end
 
--- test/tc_gli.rb
+++ test/tc_gli.rb
@@ -6,33 +6,45 @@ include GLI
 class TC_testGLI < Test::Unit::TestCase
 
   def test_flag_create
+    do_test_flag_create(GLI)
+    do_test_flag_create(Command.new(:f,'Some command'))
+  end
+  def do_test_flag_create(object)
     description = 'this is a description'
-    GLI.desc description
-    arg_name 'filename'
-    default_value '~/.blah.rc'
-    flag :f
-    assert (flags[:f] )
-    assert_equal(description,flags[:f].description)
-    assert_equal("-f filename - #{description} (default ~/.blah.rc)",flags[:f].usage)
+    object.desc description
+    object.arg_name 'filename'
+    object.default_value '~/.blah.rc'
+    object.flag :f
+    assert (object.flags[:f] )
+    assert_equal(description,object.flags[:f].description)
+    assert_equal("-f filename - #{description} (default ~/.blah.rc)",object.flags[:f].usage)
   end
 
   def test_switch_create
+    do_test_switch_create(GLI)
+    do_test_switch_create(Command.new(:f,'Some command'))
+  end
+  def do_test_switch_create(object)
     description = 'this is a description'
-    GLI.desc description
-    switch :f
-    assert (switches[:f] )
-    assert_equal(description,switches[:f].description)
+    object.desc description
+    object.switch :f
+    assert (object.switches[:f] )
+    assert_equal(description,object.switches[:f].description)
   end
 
   def test_switch_create_twice
+    do_test_switch_create_twice(GLI)
+    do_test_switch_create_twice(Command.new(:f,'Some command'))
+  end
+  def do_test_switch_create_twice(object)
     description = 'this is a description'
-    GLI.desc description
-    switch :f
-    assert (switches[:f] )
-    assert_equal(description,switches[:f].description)
-    switch :g
-    assert (switches[:g])
-    assert_equal(nil,switches[:g].description)
+    object.desc description
+    object.switch :f
+    assert (object.switches[:f] )
+    assert_equal(description,object.switches[:f].description)
+    object.switch :g
+    assert (object.switches[:g])
+    assert_equal(nil,object.switches[:g].description)
   end
 
   def test_command_create
