e0d663af27e470595bd5d6a9e4536af402e8f6db
Dave Copeland <davetron5000@gmail.com>
2013-04-15 08:43:03 -0400

    Oh 1.8.7.  No capitalize on symbols

--- lib/gli/gli_option_parser.rb
+++ lib/gli/gli_option_parser.rb
@@ -10,7 +10,7 @@ module GLI
 
     # Given the command-line argument array, returns an OptionParsingResult
     def parse_options(args) # :nodoc:
-      option_parser_class = self.class.const_get("#{@subcommand_option_handling_strategy.capitalize}CommandOptionParser")
+      option_parser_class = self.class.const_get("#{@subcommand_option_handling_strategy.to_s.capitalize}CommandOptionParser")
       OptionParsingResult.new.tap { |parsing_result|
         parsing_result.arguments = args
         parsing_result = @global_option_parser.parse!(parsing_result)
