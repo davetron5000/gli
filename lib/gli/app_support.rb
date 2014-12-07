b5bd8fa3e061cba5986f6eba1dccce0178349749
Dave Copeland <davec@naildrivin5.com>
2014-12-07 14:36:38 -0500

    move command to public and set @app

--- lib/gli/app_support.rb
+++ lib/gli/app_support.rb
@@ -32,10 +32,6 @@ module GLI
       clear_nexts
     end
 
-    def exe_name
-      File.basename($0)
-    end
-
     # Get an array of commands, ordered by when they were declared
     def commands_declaration_order # :nodoc:
       @commands_declaration_order
