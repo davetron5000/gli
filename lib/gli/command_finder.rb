03fec166de1a9e97faf298a015d212e67c86c0a6
ChrisArcand <chris@chrisarcand.com>
2015-02-26 16:51:56 -0500

    Simplify #initialize, clarify intent
    Delegate some work to a helper method; also I think
    commands_with_aliases is a bit more helpful to read, the intent wasn't
    really coming to me quickly when I first read it.

--- lib/gli/command_finder.rb
+++ lib/gli/command_finder.rb
@@ -1,7 +1,6 @@
 module GLI
   class CommandFinder
     attr_accessor :options
-    attr_accessor :names_to_commands
 
     DEFAULT_OPTIONS = {
       default_command: nil
@@ -9,26 +8,17 @@ module GLI
 
     def initialize(commands, options = {})
       self.options = DEFAULT_OPTIONS.merge(options)
-      self.names_to_commands = {}
-
-      commands.each do |command_name, command|
-        names_to_commands[command_name.to_s] = command
-        Array(command.aliases).each do |command_alias|
-          names_to_commands[command_alias.to_s] = command
-        end
-      end
+      self.commands_with_aliases = expand_with_aliases(commands)
     end
 
-    # Finds the command with the given name, allowing for partial matches.  Returns the command named by
-    # the default command if no command with +name+ matched
     def find_command(name)
       name = String(name || options[:default_command]).strip
-
       raise UnknownCommand.new("No command name given nor default available") if name == ''
 
-      command_found = names_to_commands.fetch(name) do |command_to_match|
-        find_command_by_partial_name(names_to_commands, command_to_match)
+      command_found = commands_with_aliases.fetch(name) do |command_to_match|
+        find_command_by_partial_name(commands_with_aliases, command_to_match)
       end
+
       if Array(command_found).empty?
         raise UnknownCommand.new("Unknown command '#{name}'")
       elsif command_found.kind_of? Array
@@ -38,10 +28,22 @@ module GLI
     end
 
   private
+    attr_accessor :commands_with_aliases
 
-    def find_command_by_partial_name(names_to_commands, command_to_match)
-      partial_matches = names_to_commands.keys.select { |command_name| command_name =~ /^#{command_to_match}/ }
-      return names_to_commands[partial_matches[0]] if partial_matches.size == 1
+    def expand_with_aliases(commands)
+      expanded = {}
+      commands.each do |command_name, command|
+        expanded[command_name.to_s] = command
+        Array(command.aliases).each do |command_alias|
+          expanded[command_alias.to_s] = command
+        end
+      end
+      expanded
+    end
+
+    def find_command_by_partial_name(commands_with_aliases, command_to_match)
+      partial_matches = commands_with_aliases.keys.select { |command_name| command_name =~ /^#{command_to_match}/ }
+      return commands_with_aliases[partial_matches[0]] if partial_matches.size == 1
       partial_matches
     end
   end
