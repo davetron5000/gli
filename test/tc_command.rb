e90b9884f1982d5dc59fd23e66a1b59659068daa
Anna Fowles-Winkler <anna.fowles-winkler@oracle.com>
2015-08-27 10:56:37 -0600

    Fix syntax of method call

--- test/tc_command.rb
+++ test/tc_command.rb
@@ -397,7 +397,7 @@ class TC_testCommand < Clean::Test::TestCase
     @app.pre { |*| false }
 
     assert_equal 65,@app.run(["bs"]) # BSD for "input data incorrect in some way"
-    assert_contained 'error: preconditions failed',@fake_stderr.to_s
+    assert_contained(@fake_stderr,/error: preconditions failed/)
     assert_equal '',@fake_stdout.to_s
   end
 
