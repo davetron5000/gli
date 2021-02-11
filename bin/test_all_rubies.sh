56f2ec1dc5eebb7fb81fec01bc85c6ac19d72e3b
Dave Copeland <davec@naildrivin5.com>
2021-02-10 20:38:18 -0500

    remove unused script

--- bin/test_all_rubies.sh
+++
@@ -1,6 +0,0 @@
-echo tests
-rvm 1.9.2@gli-dev,1.9.3@gli-dev,1.8.7@gli-dev,jruby@gli-dev,rbx@gli-dev,ree@gli-dev --yaml rake test | bin/report_on_rake_results
-rake clobber > /dev/null 2>&1
-echo features
-rvm 1.9.2@gli-dev,1.9.3@gli-dev,1.8.7@gli-dev,jruby@gli-dev,rbx@gli-dev,ree@gli-dev --yaml rake features | bin/report_on_rake_results
-rake clobber > /dev/null 2>&1
